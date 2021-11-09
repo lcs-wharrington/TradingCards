25d20a1a6d5423d455f5feb8b906afc1653c5911
Will Harrington <WHarrington>
2021-11-09 12:02:05 -0500

    Now have all players listed

--- TradingCards/Model/AvalanchePlayer.swift
+++ TradingCards/Model/AvalanchePlayer.swift
@@ -20,7 +20,16 @@ struct AvalanchePlayer: Identifiable {
 let players = [
 
     AvalanchePlayer(name: "Nathan, MacKinnon", imageName: "Nathan MacKinnon", careerGoals: "210", position: "Centerman"),
-
+    
+    AvalanchePlayer(name: "Mikko, Rantanen", imageName: "Mikko Rantanen", careerGoals: "340", position: "Right Wing"),
+    
+    AvalanchePlayer(name: "Gabriel, Landeskog", imageName: "Gabriel Landeskog", careerGoals: "695", position: "Left Wing"),
+    
+    AvalanchePlayer(name: "Samuel, Girard", imageName: "Samuel Girard", careerGoals: "281", position: "Deffense"),
+    
+    AvalanchePlayer(name: "Kurtis, MacDermid", imageName: "Kurtis MacDermid", careerGoals: "123", position: "Deffense"),
+    
+    AvalanchePlayer(name: "Darcy, Kuemper", imageName: "Darcy, Kuemper", careerGoals: "250", position: "Goalie"),
 ]
 
 
